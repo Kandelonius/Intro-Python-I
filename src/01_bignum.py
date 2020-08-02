70001d2bc19e6afb914b8e0f4e91f5db09d29344
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-02 14:52:01 -0700

    completed the first 5 sections so far.

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,7 @@
+import random, math
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+# print(math.pow(2, 65536)) # OverflowError: math range error
+print(f"2^50 is {math.pow(2, 50)}")
+print(f"Some unreasonably long number is {2**65536}")
